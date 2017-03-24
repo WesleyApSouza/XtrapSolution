ecf36808d55471ec6b4551f7d4984aed4661f123
Wesley Aparecido de Souza <wesley_souza1996@hotmail.com>
2017-03-24 12:57:01 -0300

    solution for view

---
+++ XtrapSolution/Controllers/CadastroController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XtrapSolution.Controllers
+{
+    public class CadastroController : Controller
+    {
+        // GET: Cadastro
+        public ActionResult CadastroProdutos()
+        {
+            ViewBag.Title = "CadastroProdutos";
+            return View();
+        }
+        public ActionResult CadastroFornecedores()
+        {
+            ViewBag.Title = "CadastroFornecedores";
+            return View();
+        }
+        public ActionResult CadastroClientes()
+        {
+            ViewBag.Title = "CadastroClientes";
+            return View();
+        }
+    }
+}
---
+++ XtrapSolution/Controllers/ListasController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XtrapSolution.Controllers
+{
+    public class ListasController : Controller
+    {
+        // GET: Lista
+        public ActionResult ListaProdutos()
+        {
+            ViewBag.Title = "ListaProdutos";
+            return View();
+        }
+        public ActionResult ListaFornecedores()
+        {
+            ViewBag.Title = "ListaFornecedores";
+            return View();
+        }
+        public ActionResult ListaClientes()
+        {
+            ViewBag.Title = "ListaClientes";
+            return View();
+        }
+    }
+}
